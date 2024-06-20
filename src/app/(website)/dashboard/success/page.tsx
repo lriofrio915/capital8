2e46e68631debec8a927657d17a41134076e55ce
lriofrio915 <lriofrio915@gmail.com>
2024-06-20 13:34:04 -0400

    se prueba el git ssh 3

--- src/app/(website)/dashboard/success/page.tsx
+++ src/app/(website)/dashboard/success/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
       <Box>
         <div className="flex mt-24  justify-center h-screen">
           <div className="flex flex-col space-y-4">
-            <p>Gracias Por Unirte!!</p>
+            <p>Gracias Por Unirte!!!</p>
             <Link href={"/dashboard"}>Regresar al Panel de Usuario</Link>
           </div>
         </div>
