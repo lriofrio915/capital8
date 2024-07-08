5f6423c688c0fa53b9a513680ead222167cefb16
lriofrio915 <lriofrio915@gmail.com>
2024-07-08 12:26:49 -0500

    se actualizan estilos del susbscription profile 3

--- src/components/SubscriptionProfile.tsx
+++ src/components/SubscriptionProfile.tsx
@@ -6,7 +6,7 @@ import { FiCheckCircle } from "react-icons/fi";
 const SubscriptionProfile: React.FC = () => {
   return (
     <div className="flex flex-col items-center py-20 min-h-screen">
-      <div className="bg-white shadow-lg rounded-xl p-10 max-w-6xl text-center">
+      <div className="bg-white shadow-lg rounded-xl p-10 w-full max-w-6xl text-center">
         <h1 className="text-4xl font-extrabold text-gray-800 mb-6">
           Bienvenido a Capital 8
         </h1>
@@ -15,8 +15,8 @@ const SubscriptionProfile: React.FC = () => {
           nuestro soporte (soporte@capital8tech.com) con tus credenciales de
           MetaTrader.
         </p>
-        <div className="flex flex-row justify-center items-start gap-10">
-          <div className="p-4 w-full max-w-sm">
+        <div className="flex flex-col md:flex-row justify-center items-start gap-10 w-full">
+          <div className="p-4 w-full">
             <div className="h-full p-6 rounded-lg border-2 border-green-500 flex flex-col relative overflow-hidden">
               <span className="bg-green-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
                 CUENTA CLÁSICA
@@ -30,36 +30,39 @@ const SubscriptionProfile: React.FC = () => {
                   className="mb-4"
                 />
               </div>
-              <p className="flex items-center text-gray-600 mb-2">
-                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <FiCheckCircle size={16} color="#fff" />
-                </span>
-                Inversión mínima 500 dólares americanos
-              </p>
-              <p className="flex items-center text-gray-600 mb-2">
-                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <FiCheckCircle size={16} color="#fff" />
-                </span>
-                Visión a mediano y largo plazo
-              </p>
-              <p className="flex items-center text-gray-600 mb-2">
-                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <FiCheckCircle size={16} color="#fff" />
-                </span>
-                Capital 8 no tiene acceso al retiro de tus fondos
-              </p>
-              <p className="flex items-center text-gray-600 mb-2">
-                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <FiCheckCircle size={16} color="#fff" />
-                </span>
-                Puedes incrementar tu inversión o retirar el capital libremente
-              </p>
-              <p className="flex items-center text-gray-600 mb-6">
-                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <FiCheckCircle size={16} color="#fff" />
-                </span>
-                Acompañamiento personalizado vía WhatsApp
-              </p>
+              <div className="flex flex-col items-start text-left">
+                <p className="flex items-center text-gray-600 mb-2">
+                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <FiCheckCircle size={16} color="#fff" />
+                  </span>
+                  Inversión mínima 500 dólares americanos
+                </p>
+                <p className="flex items-center text-gray-600 mb-2">
+                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <FiCheckCircle size={16} color="#fff" />
+                  </span>
+                  Visión a mediano y largo plazo
+                </p>
+                <p className="flex items-center text-gray-600 mb-2">
+                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <FiCheckCircle size={16} color="#fff" />
+                  </span>
+                  Capital 8 no tiene acceso al retiro de tus fondos
+                </p>
+                <p className="flex items-center text-gray-600 mb-2">
+                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <FiCheckCircle size={16} color="#fff" />
+                  </span>
+                  Puedes incrementar tu inversión o retirar el capital
+                  libremente
+                </p>
+                <p className="flex items-center text-gray-600 mb-6">
+                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <FiCheckCircle size={16} color="#fff" />
+                  </span>
+                  Acompañamiento personalizado vía WhatsApp
+                </p>
+              </div>
               <Link
                 href="https://bit.ly/3ziIrph"
                 target="_blank"
@@ -69,9 +72,9 @@ const SubscriptionProfile: React.FC = () => {
               </Link>
             </div>
           </div>
-          <div className="p-4 w-full max-w-sm">
-            <div className="h-full p-6 rounded-lg border-2 border-green-500 flex flex-col relative overflow-hidden">
-              <span className="bg-green-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
+          <div className="p-4 w-full">
+            <div className="h-full p-6 rounded-lg border-2 border-orange-500 flex flex-col relative overflow-hidden">
+              <span className="bg-orange-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
                 CUENTA D-ZERO
               </span>
               <div className="flex flex-col items-center">
@@ -83,40 +86,42 @@ const SubscriptionProfile: React.FC = () => {
                   className="mb-4"
                 />
               </div>
-              <p className="flex items-center text-gray-600 mb-2">
-                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <FiCheckCircle size={16} color="#fff" />
-                </span>
-                Inversión mínima 500 dólares americanos
-              </p>
-              <p className="flex items-center text-gray-600 mb-2">
-                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <FiCheckCircle size={16} color="#fff" />
-                </span>
-                Visión a mediano y largo plazo
-              </p>
-              <p className="flex items-center text-gray-600 mb-2">
-                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <FiCheckCircle size={16} color="#fff" />
-                </span>
-                Capital 8 no tiene acceso al retiro de tus fondos
-              </p>
-              <p className="flex items-center text-gray-600 mb-2">
-                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <FiCheckCircle size={16} color="#fff" />
-                </span>
-                Puedes incrementar tu inversión o retirar el capital libremente
-              </p>
-              <p className="flex items-center text-gray-600 mb-6">
-                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <FiCheckCircle size={16} color="#fff" />
-                </span>
-                Acompañamiento personalizado vía WhatsApp
-              </p>
+              <div className="flex flex-col items-start text-left">
+                <p className="flex items-center text-gray-600 mb-2">
+                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <FiCheckCircle size={16} color="#fff" />
+                  </span>
+                  Crea un historial exitoso de trading y atrae inversores
+                </p>
+                <p className="flex items-center text-gray-600 mb-2">
+                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <FiCheckCircle size={16} color="#fff" />
+                  </span>
+                  Visión a largo plazo
+                </p>
+                <p className="flex items-center text-gray-600 mb-2">
+                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <FiCheckCircle size={16} color="#fff" />
+                  </span>
+                  Capital 8 no tiene acceso al retiro de tus fondos
+                </p>
+                <p className="flex items-center text-gray-600 mb-2">
+                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <FiCheckCircle size={16} color="#fff" />
+                  </span>
+                  Ganas comisiones sobre el capital asignado por los inversores
+                </p>
+                <p className="flex items-center text-gray-600 mb-6">
+                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <FiCheckCircle size={16} color="#fff" />
+                  </span>
+                  Acompañamiento personalizado vía WhatsApp
+                </p>
+              </div>
               <Link
                 href="https://bit.ly/4cwGovX"
                 target="_blank"
-                className="flex items-center text-center justify-center mt-auto text-white bg-green-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-green-600 rounded"
+                className="flex items-center text-center justify-center mt-auto text-white bg-orange-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-orange-600 rounded"
               >
                 Suscribirse
               </Link>
