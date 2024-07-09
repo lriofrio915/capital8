17926e5c652b1e4bad5a4bea37d48bf30b3f3449
lriofrio915 <lriofrio915@gmail.com>
2024-07-09 06:02:43 -0500

    se actualizan estilos del no-susbscription profile

--- src/components/NoSubscriptionProfile.tsx
+++ src/components/NoSubscriptionProfile.tsx
@@ -32,57 +32,45 @@ const NoSubscriptionProfile: React.FC = async () => {
   );
 
   return (
-    <div className="flex flex-col items-center py-20 min-h-screen bg-gray-50">
-      <div className="bg-white shadow-lg rounded-xl p-10 max-w-2xl text-center">
-        <h1 className="text-4xl font-extrabold text-gray-800 mb-6">
-          Considera Suscribirte
-        </h1>
-        <p className="text-gray-700 mb-8">
-          En Capital 8 ofrecemos una gestión experta sin necesidad de entregar
-          tu dinero, tendrás control total mientras nosotros nos encargamos de
-          maximizar tus rendimientos con estrategias probadas y supervisión
-          constante.
-        </p>
-        <div className="space-y-12">
-          <div>
-            <h2 className="text-2xl font-bold text-gray-800 mb-4">
-              Detalles del servicio:
-            </h2>
-            <ul className="list-disc list-inside text-gray-700 mb-6">
-              <li>El tamaño mínimo de la cuenta a gesionar es de 1000 usd.</li>
-              <li>
-                Puedes realizar retiros o aumentar el capital cada que lo
-                desees.
-              </li>
-              <li>
-                El riesgo máximo asumido es el 4% del capital por cada
-                operación.
-              </li>
-              <li>Jornada operativa de lunes a viernes: 9:30 am - 11:00 am.</li>
-              <li>
-                Se ejecuta una sola operación al día y no todos los días hay
-                operaciones.
-              </li>
-              <li>
-                El capital se invierte en el índice Nasdaq mediante Futuros y
-                CFDs.
-              </li>
-              <li>La tasa de efectividad promedio supera el 60%.</li>
-            </ul>
+    <div className="bg-white mt-6 shadow-lg rounded-xl p-10 max-w-2xl text-center">
+      <h1 className="text-4xl font-extrabold text-gray-800 mb-6">
+        Considera Suscribirte
+      </h1>
+      <p className="text-gray-700 mb-8">
+        En Capital 8 ofrecemos una gestión experta sin necesidad de entregar tu
+        dinero, tendrás control total mientras nosotros nos encargamos de
+        maximizar tus rendimientos con estrategias probadas y supervisión
+        constante.
+      </p>
+      <div className="p-4 w-full">
+        <div className="h-full p-6 rounded-lg border-2 border-green-500 flex flex-col relative overflow-hidden">
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">
+            Detalles del Servicio
+          </h2>
+          <div className="flex flex-col items-center">
             <Image
               src="https://luis-capital8.s3.us-east-2.amazonaws.com/trading-floor.jpg"
               alt="Trading Solutions"
-              className="mx-auto mb-6"
-              width={500}
-              height={500}
+              width={400}
+              height={400}
+              className="mb-4"
             />
-            <Link
-              href={"" + checkout_link}
-              className="bg-blue-600 text-white font-semibold px-8 py-4 rounded-full hover:bg-blue-700 transition duration-300"
-            >
-              Contratar un Gestor
-            </Link>
           </div>
+          <div className="flex flex-col items-start text-left">
+            <ul className="list-disc list-inside text-gray-700 mb-6">
+              <li>Riesgo máximo por operación: 4%.</li>
+              <li>Jornada operativa: lunes a viernes de 9:30 am - 11:00 am.</li>
+              <li>
+                Inversiones intradía en el Nasdaq mediante Futuros y CFDs.
+              </li>
+            </ul>
+          </div>
+          <Link
+            href={"" + checkout_link}
+            className="flex items-center text-center justify-center mt-auto text-white bg-green-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-green-600 rounded"
+          >
+            Contratar un Gestor
+          </Link>
         </div>
       </div>
     </div>
